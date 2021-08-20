3bf605d474c7e6bc31d372e7c2922bb0a5fd30cf
Aashiish-Pandey <ash006pandey@gmail.com>
2021-08-20 10:48:24 +0530

    fetching data and handling 404 error

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,35 @@
-import React, { useState, useEffect } from 'react';
-
-const url = 'https://api.github.com/users';
+import React, { useState, useEffect } from "react";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+  const url = "https://api.github.com/users";
+  const fetchData = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setUsers(data);
+  };
+
+  console.log(users);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <ul className="users">
+      {users.map((user) => {
+        const { id, login, avatar_url, html_url } = user;
+        return (
+          <li key={id}>
+            <img src={avatar_url} alt={login} />
+            <div>
+              <h4>{login}</h4>
+              <a href ={html_url}>profile</a>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default UseEffectFetchData;
