e3a8d2997c8c1e25a1c903dfc746ccf2a507edad
Aashiish-Pandey <ash006pandey@gmail.com>
2021-08-22 12:04:15 +0530

    Ternary operator

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide'
-import Final from './tutorial/3-conditional-rendering/final/3-show-hide'
+import React from "react";
+import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
+import Final from "./tutorial/3-conditional-rendering/final/3-show-hide";
 function App() {
   return (
-    <div className='container'>
-      
+    <div className="container">
       <Setup />
       {/* <Final /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-
-  const[isError,setIsError] = useState(false)
-  return(
-
+  const [isError, setIsError] = useState(false);
+  return (
     <div>
-      <button className ="btn" onClick ={()=>setIsError(!isError)}>show/hide</button>
-      {isError && <h1>Error....</h1>}
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        show/hide
+      </button>
+      {isError ? <h1>Error....</h1> : <h1>no Error</h1>}
     </div>
-  )
-}
+  );
+};
 
 export default ShowHide;
