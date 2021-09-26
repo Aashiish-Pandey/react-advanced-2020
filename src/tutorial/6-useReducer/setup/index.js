0db00333cf8e9948f5db8c17d3b82d8279216d32
Aashiish-Pandey <ash006pandey@gmail.com>
2021-09-27 00:08:30 +0530

    useReducer hook completed

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,27 +1,43 @@
 import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
+
+import { reducer } from "./reducer";
 // reducer function
+
+const defaultState = {
+  people: data,
+  isModalOpen: true,
+  modalContent: "hello world",
+};
 
 const Index = () => {
   const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultState);
   const [people, setPeople] = useState(data);
   const [showModal, setShowModal] = useState(false);
+  console.log(state);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (name) {
-      setShowModal(true);
-      setPeople([...people, { id: new Date().getTime().toString() }]);
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newItem });
     } else {
-      setShowModal(true);
+      dispatch({ type: "NO_VALUE" });
     }
+  };
+
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
   };
 
   return (
     <>
-      {showModal && <Modal />}
+      {defaultState.isModalOpen && (
+        <Modal modalContent={state.modalContent} closeModal={closeModal} />
+      )}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -34,10 +50,18 @@ const Index = () => {
         <button type="submit">add</button>
       </form>
 
-      {people.map((person) => {
+      {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className="item">
             <h4>{person.name}</h4>
+
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: person.id })
+              }
+            >
+              remove item
+            </button>
           </div>
         );
       })}
